728e72a6f4567b199099e516f712291e05fe7f16
leslie kimm <leslie.kimm@gmail.com>
2016-02-20 20:29:21 -0800

    show link if available

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.controller('MainCtrl', ['$scope', function($scope) {
 		if(!$scope.title || $scope.title === '') { return; }
   		$scope.posts.push({
   			title: $scope.title,
+  			link: $scope.link,
   			upvotes: 0
   		});
   		$scope.title = '';
+  		$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post) {
