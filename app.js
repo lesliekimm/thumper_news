42c0ed1a45a15cc112ce553cf55a8a68ce439a0d
leslie kimm <leslie.kimm@gmail.com>
2016-02-20 20:44:00 -0800

    Comment works

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       		url: '/home',
       		templateUrl: '/home.html',
       		controller: 'MainCtrl'
-    	});
+    	})
+    	.state('posts', {
+  			url: '/posts/{id}',
+  			templateUrl: '/posts.html',
+  			controller: 'PostsCtrl'
+		});
 
   	$urlRouterProvider.otherwise('home');
 }]);
@@ -26,7 +31,11 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
   		$scope.posts.push({
   			title: $scope.title,
   			link: $scope.link,
-  			upvotes: 0
+  			upvotes: 0,
+  			comments: [
+    			{author: 'Mowgli', body: 'Hi cuzin!', upvotes: 0},
+    			{author: 'Howie', body: 'Hi brodder!', upvotes: 0}
+  			]
   		});
   		$scope.title = '';
   		$scope.link = '';
@@ -34,5 +43,23 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 
 	$scope.incrementUpvotes = function(post) {
   		post.upvotes += 1;
+	};
+}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+  		if($scope.body === '') { return; }
+  		$scope.post.comments.push({
+    		body: $scope.body,
+    		author: 'user',
+    		upvotes: 0
+  		});
+  	$scope.body = '';
+};
+
+	$scope.incrementUpvotes = function(comment) {
+  		comment.upvotes += 1;
 	};
 }]);
