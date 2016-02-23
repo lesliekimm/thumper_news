309944d192d3498091a794ceedb93156c2cef995
leslie kimm <leslie.kimm@gmail.com>
2016-02-23 07:29:58 -0800

    create register/login pages

--- thumper-news/public/javascripts/angularApp.js
+++ thumper-news/public/javascripts/angularApp.js
@@ -22,8 +22,79 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
           }]
         }
       });
+      .state('login', {
+        url: '/login',
+        templateUrl: '/login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function($state, auth) {
+          if(auth.isLoggedIn()) {
+            $state.go('home');
+          }
+        }]
+      });
+      .state('register', {
+        url: '/register',
+        templateUrl: '/register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'auth', function($state, auth) {
+          if(auth.isLoggedIn()) {
+            $state.go('home');
+          }
+        }]
+      });
 
   	$urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+   var auth = {};
+
+  auth.saveToken = function (token) {
+    $window.localStorage['thumper-news-token'] = token;
+  };
+
+  auth.getToken = function () {
+    return $window.localStorage['thumper-news-token'];
+  }
+
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+    }
+    else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user) {
+    return $http.post('/register', user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user) {
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function() {
+    $window.localStorage.removeItem('thumper-news-token');
+  };
+
+  return auth;
 }]);
 
 app.factory('posts', ['$http', function($http) {
@@ -106,3 +177,23 @@ app.controller('PostsCtrl', ['$scope', 'posts','post', function($scope, posts, p
   		posts.upvoteComment(post, comment);
 	};
 }]);
+
+app.controller('AuthCtrl', ['$scope', '$state', 'auth', function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function() {
+    auth.register($scope.user).error(function(error) {
+      $scope.error = error;
+    }).then(function() {
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function() {
+    auth.logIn($scope.user).error(function(error) {
+      $scope.error = error;
+    }).then(function() {
+      $state.go('home');
+    });
+  };
+}])
