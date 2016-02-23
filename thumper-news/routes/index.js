3058b01566a03dad5913c3f5068748c078c73b93
leslie kimm <leslie.kimm@gmail.com>
2016-02-23 06:58:13 -0800

    backend can register & authenticate users

--- thumper-news/routes/index.js
+++ thumper-news/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var passport = require('passport');
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
@@ -20,8 +22,9 @@ router.get('/posts', function(req, res, next) {
   	});
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   	var post = new Post(req.body);
+  	post.author = req.payload.username;
 
   	post.save(function(err, post) {
     	if(err){ return next(err); }
@@ -49,7 +52,7 @@ router.get('/posts/:post', function(req, res) {
 	});
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   	req.post.upvote(function(err, post){
     	if (err) { return next(err); }
 
@@ -57,9 +60,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   	});
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
+	comment.author = req.payload.username;
 
 	comment.save(function(err, comment) {
 		if (err) { return next(err); }
@@ -84,7 +88,7 @@ router.param('comment', function(req, res, next, id) {
   	});
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   	req.comment.upvote(function(err, comment){
     	if (err) { return next(err); }
 
