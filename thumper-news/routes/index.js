8a97ed128d025bdacb3237b365dcbe7c6bb78757
leslie kimm <leslie.kimm@gmail.com>
2016-02-23 19:55:53 -0800

    require express-jwt

--- thumper-news/routes/index.js
+++ thumper-news/routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var jwt = require('express-jwt');
+var auth = jwt({ secret: 'SECRET', userProperty: 'payload' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
