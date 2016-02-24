206145fe92555f5d9d31e8516c715e7858ec1504
leslie kimm <leslie.kimm@gmail.com>
2016-02-23 19:54:38 -0800

    add login route

--- thumper-news/routes/index.js
+++ thumper-news/routes/index.js
@@ -96,6 +96,22 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   	});
 });
 
+/* POST login */
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
 
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+
+        if(user) {
+            return res.json({ token: user.generateJWT() });
+        }
+        else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+});
 
 module.exports = router;
