162912abeeefddb022b141f2af6123472476bd7c
leslie kimm <leslie.kimm@gmail.com>
2016-02-23 20:11:39 -0800

    define auth factory methods

--- thumper-news/public/javascripts/angularApp.js
+++ thumper-news/public/javascripts/angularApp.js
@@ -70,6 +70,56 @@ app.factory('posts', ['$http', function($http) {
   	return o;
 }]);
 
+app.factory('auth', ['$http', '$window', function($http, $window) {
+    var auth = {};
+
+    auth.saveToken = function (token) {
+        $window.localStorage['thumper-news-token'] = token;
+    };
+
+    auth.getToken = function () {
+        return $window.localStorage['thumper-news-token'];
+    }
+
+    auth.isLoggedIn = function() {
+        var token = auth.getToken();
+
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.exp > Date.now() / 1000;
+        }
+        else {
+            return false;
+        }
+    };
+
+    auth.currentUser = function() {
+        if (auth.isLoggedIn()) {
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.username;
+        }
+    };
+
+    auth.register = function(user) {
+        return $http.post('/register', user).success(function(data) {
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logIn = function(user) {
+        return $http.post('/login', user).success(function(data) {
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logOut = function() {
+        $window.localStorage.removeItem('thumper-news-token');
+    };
+
+    return auth;
+}])
+
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
     $scope.posts = posts.posts;
 
